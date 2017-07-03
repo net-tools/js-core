62da329752bb6b96dc049f6028d75ad5bb3d4e70
Pierre <dev@net-tools.ovh>
2017-07-03 17:07:52 +0200

    bugfix jsdoc

--- src/js-core.js
+++ src/js-core.js
@@ -1298,7 +1298,7 @@ nettools.jscore.RequestHelper = (function(){
          * Sending an XmlHttpRequest
          * 
          * @param string url
-         * @param function(xmlhttpresponse) callback
+         * @param function(xmlhttpobject) callback
          * @param string|Object postData
          */
 		sendXmlHTTPRequest : function(url,callback,postData)
